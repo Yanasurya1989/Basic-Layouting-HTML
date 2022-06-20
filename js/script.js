688aa1916f7f95b381f6bb490a258c48d09a5525
Yanasurya1989 <yanasurya1989@gmail.com>
2022-06-20 12:58:20 +0700

    Pushed to Github

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function clickHome(){
+    alert('Welcome in My Home');
+}
+
+function clickBlog(){
+    alert('welcome in My Blog')
+}
+
+function clickCourse(){
+    alert('Choose your course')
+}
+
+function clickAbout(){
+    alert('what did you interes for me');
+}
